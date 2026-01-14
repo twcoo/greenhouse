09a08b9e121b1f1fec8d10848f714a1cc9ae9f31
twcoo <mflesterc@gmail.com>
2026-01-15 05:24:05 +0800

    feat: register models

--- app/greenhouse/models/__init__.py
+++ app/greenhouse/models/__init__.py
@@ -1,2 +1,11 @@
 from .crop import Crop as Crop
+from .planting import Planting as Planting
+from .planting_daily_observation import \
+    PlantingDailyObservation as PlantingDailyObservation
+from .planting_growth_stage import PlantingGrowthStage as PlantingGrowthStage
+from .planting_location import PlantingLocation as PlantingLocation
+from .planting_location_assignment import \
+    PlantingLocationAssignment as PlantingLocationAssignment
+from .planting_location_status import \
+    PlantingLocationStatus as PlantingLocationStatus
 from .variety import Variety as Variety
