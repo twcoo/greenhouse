49b8c0030d0ca3197fb7556f9149370cbd71d48c
twcoo <mflesterc@gmail.com>
2026-01-15 16:48:42 +0800

    feat: add planting models

--- app/greenhouse/admin.py
+++ app/greenhouse/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin as admin
 
-from .models import Crop as Crop
-from .models import Variety as Variety
+from .models import (Crop, Planting, PlantingDailyObservation,
+                     PlantingGrowthStage, PlantingLocation,
+                     PlantingLocationAssignment, PlantingLocationStatus,
+                     Variety)
 
 admin.site.register(Crop)
 admin.site.register(Variety)
+admin.site.register(Planting)
+admin.site.register(PlantingDailyObservation)
+admin.site.register(PlantingGrowthStage)
+admin.site.register(PlantingLocation)
+admin.site.register(PlantingLocationAssignment)
+admin.site.register(PlantingLocationStatus)
