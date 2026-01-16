748aba0e2715cdc72f5a6634c47c9ff1b4a1e4e8
twcoo <mflesterc@gmail.com>
2026-01-16 16:09:09 +0800

    feat: initial drf spectacular setup

--- app/core/settings.py
+++ app/core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.postgres",
     "rest_framework",
     "greenhouse",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -109,6 +110,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Greenhouse",
+    "DESCRIPTION": "",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
