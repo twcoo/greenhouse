a79a901f1c4a4bb57be6dcb548c67dc9da1347e2
twcoo <mflesterc@gmail.com>
2026-01-17 17:29:31 +0800

    feat: add api auth url

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
+    path("api/auth/", include("greenhouse.urls")),
 ]
