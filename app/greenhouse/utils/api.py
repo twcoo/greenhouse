0fa8a265a1a59689689d4b4547b965ceed355056
twcoo <mflesterc@gmail.com>
2026-01-22 01:33:08 +0800

    chore: remove schema class

--- app/greenhouse/utils/api.py
+++ app/greenhouse/utils/api.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from typing import Any, Mapping, Optional
 
+from knox.auth import TokenAuthentication
 from rest_framework.response import Response
 
 
@@ -37,61 +38,5 @@ class CustomResponse(Response):
         super().__init__(formatted_data, **kwargs)
 
 
-# Class for building the OpenAPI schema with the structure of JSend
-# Reference: https://github.com/omniti-labs/jsend
-class CustomOpenAPIResponseSchema:
-    def __init__(
-        self,
-        response_status_example: str = "success",
-        response_data_properties: Optional[Mapping[str, Any]] = None,
-        response_message_example: Optional[str] = None,
-        response_data_required_properties: Optional[list[str]] = None,
-    ):
-        """
-        Initialize the CustomOpenAPIResponseSchema.
-
-        :param str response_status_example: Example value for the 'status' field.
-        :param Optional[Mapping[str, Any]] response_data_properties: Schema properties for the 'data' field.
-        :param Optional[str] response_message_example: Example value for the 'message' field.
-        :param Optional[list[str]] response_data_required_properties: List of required properties for the 'data' field.
-        """
-        self.response_status_example = Status(response_status_example).value
-        self.response_data_properties = response_data_properties
-        self.response_message_example = response_message_example
-        self.response_data_required_properties = (
-            response_data_required_properties
-        )
-
-    def get_schema(self) -> Mapping[str, Any]:
-        """
-        Get the OpenAPI schema for the response.
-
-        :return: A dictionary representing the schema.
-        :rtype: Mapping[str, Any]
-        """
-        return {
-            "type": "object",
-            "properties": {
-                "status": {
-                    "type": "string",
-                    "description": "The status of the API response, indicating success or failure.",
-                    "example": self.response_status_example,
-                },
-                "data": {
-                    "type": ["object", None],
-                    "description": "An object containing the data returned by the API.",
-                    **(
-                        {"properties": self.response_data_properties}
-                        if self.response_data_properties
-                        else {}
-                    ),
-                    "required": self.response_data_required_properties,
-                },
-                "message": {
-                    "type": ["string", None],
-                    "description": "Optional message providing additional information about the response.",
-                    "example": self.response_message_example,
-                },
-            },
-            "required": ["status", "data", "message"],
-        }
+class CustomAuthentication(TokenAuthentication):
+    pass
