82d36acebc8e3d5645bc25247e87bbf8612759a2
twcoo <mflesterc@gmail.com>
2026-01-22 01:43:11 +0800

    feat: add custom custom authentication

--- app/greenhouse/views/auth.py
+++ app/greenhouse/views/auth.py
@@ -7,10 +7,12 @@ from knox.views import LogoutView as KnoxLogoutView
 from rest_framework import status
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.permissions import AllowAny
+from rest_framework.request import Request
 from rest_framework.views import APIView
 
+from ..schemas import CustomOpenAPIResponseSchema
 from ..serializers import RegisterSerializer
-from ..utils.api import CustomOpenAPIResponseSchema, CustomResponse
+from ..utils.api import CustomAuthentication, CustomResponse
 
 
 @extend_schema(
@@ -33,7 +35,7 @@ from ..utils.api import CustomOpenAPIResponseSchema, CustomResponse
                         "description": "Authentication token used for API requests.",
                     },
                 },
-                response_data_required_properties=["access_token"],
+                response_data_required_properties=["expiry", "token"],
             ).get_schema(),
             examples=[
                 OpenApiExample(
@@ -89,7 +91,7 @@ from ..utils.api import CustomOpenAPIResponseSchema, CustomResponse
 class RegisterView(APIView):
     permission_classes = [AllowAny]
 
-    def post(self, request):
+    def post(self, request: Request) -> CustomResponse:
         serializer = RegisterSerializer(data=request.data)
 
         if serializer.is_valid():
@@ -116,7 +118,7 @@ class RegisterView(APIView):
     auth=[],
     operation_id="Login",
     tags=["Authentication"],
-    description="Allows new users to login by providing their details.",
+    description="Allows users to login by providing their details.",
     request=AuthTokenSerializer,
     responses={
         200: OpenApiResponse(
@@ -132,11 +134,11 @@ class RegisterView(APIView):
                         "description": "Authentication token used for API requests.",
                     },
                 },
-                response_data_required_properties=["access_token"],
+                response_data_required_properties=["expiry", "token"],
             ).get_schema(),
             examples=[
                 OpenApiExample(
-                    name="Successful registration",
+                    name="Successful login",
                     status_codes=["200"],
                     response_only=True,
                     value={
@@ -146,6 +148,25 @@ class RegisterView(APIView):
                             "token": "2d54d895a9aaa0d4bd9d8052579e280e6e0da24f2.......",
                         },
                         "message": None,
+                    },
+                ),
+            ],
+        ),
+        400: OpenApiResponse(
+            description="Bad Request",
+            response=CustomOpenAPIResponseSchema().get_schema(),
+            examples=[
+                OpenApiExample(
+                    name="Username and password fields are required",
+                    status_codes=["400"],
+                    response_only=True,
+                    value={
+                        "status": "error",
+                        "data": None,
+                        "message": {
+                            "username": ["This field is required."],
+                            "password": ["This field is required."],
+                        },
                     },
                 ),
             ],
@@ -161,11 +182,7 @@ class RegisterView(APIView):
                     value={
                         "status": "error",
                         "data": None,
-                        "message": {
-                            "non_field_errors": [
-                                "Unable to log in with provided credentials."
-                            ]
-                        },
+                        "message": "Unable to log in with provided credentials.",
                     },
                 ),
             ],
@@ -175,14 +192,14 @@ class RegisterView(APIView):
 class LoginView(KnoxLoginView):
     permission_classes = [AllowAny]
 
-    def post(self, request, format=None):
+    def post(self, request: Request, format: None = None) -> CustomResponse:
         serializer = AuthTokenSerializer(data=request.data)
 
         if not serializer.is_valid():
             return CustomResponse(
                 response_status="error",
                 response_message=serializer.errors,
-                status=status.HTTP_401_UNAUTHORIZED,
+                status=status.HTTP_400_BAD_REQUEST,
             )
 
         user = serializer.validated_data["user"]
@@ -196,8 +213,50 @@ class LoginView(KnoxLoginView):
         )
 
 
+@extend_schema(
+    operation_id="Logout",
+    tags=["Authentication"],
+    description="Allows users to logout by providing their details.",
+    request=AuthTokenSerializer,
+    responses={
+        200: OpenApiResponse(
+            description="Success",
+            response=CustomOpenAPIResponseSchema().get_schema(),
+            examples=[
+                OpenApiExample(
+                    name="Successful logout",
+                    status_codes=["200"],
+                    response_only=True,
+                    value={
+                        "status": "success",
+                        "data": None,
+                        "message": "Logged out successfully.",
+                    },
+                ),
+            ],
+        ),
+        401: OpenApiResponse(
+            description="Unauthorized",
+            response=CustomOpenAPIResponseSchema().get_schema(),
+            examples=[
+                OpenApiExample(
+                    name="Invalid provided credentials",
+                    status_codes=["401"],
+                    response_only=True,
+                    value={
+                        "status": "error",
+                        "data": None,
+                        "message": "Unable to log in with provided credentials.",
+                    },
+                ),
+            ],
+        ),
+    },
+)
 class LogoutView(KnoxLogoutView):
-    def get_post_response(self, request):
+    authentication_classes = [CustomAuthentication]
+
+    def get_post_response(self, request: Request) -> CustomResponse:
         return CustomResponse(
             response_message="Logged out successfully.",
             status=status.HTTP_200_OK,
