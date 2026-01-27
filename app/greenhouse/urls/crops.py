2f51db53c85f066a9be33970733e82a7ece9c94b
twcoo <mflesterc@gmail.com>
2026-01-28 04:03:38 +0800

    feat: crop crud endpoints

--- app/greenhouse/urls/crops.py
+++ app/greenhouse/urls/crops.py
@@ -3,5 +3,8 @@ from django.urls import path
 from ..views import CropDetailAPIView
 
 urlpatterns = [
+    # List and create
     path("", CropDetailAPIView.as_view(), name="crop-list-create"),
+    # Get, partial update, full update, and delete
+    path("<int:pk>", CropDetailAPIView.as_view(), name="crop-detail"),
 ]
