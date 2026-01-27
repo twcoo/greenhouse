0b5836ae361e71d9ef87db194ea5e8005635a4d7
twcoo <mflesterc@gmail.com>
2026-01-28 04:04:02 +0800

    feat: add put, patch and delete endpoints

--- app/greenhouse/views/crops.py
+++ app/greenhouse/views/crops.py
@@ -1,4 +1,6 @@
-from rest_framework import status
+from typing import Any
+
+from rest_framework import mixins, status
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
@@ -8,16 +10,48 @@ from ..serializers import CropSerializer
 from ..utils.api import CustomAuthentication, CustomResponse
 
 
-class CropDetailAPIView(GenericAPIView):
+class CropDetailAPIView(
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    GenericAPIView,
+):
     authentication_classes = [CustomAuthentication]
     permission_classes = [IsAuthenticated]
-    serializer_class = CropSerializer
 
+    serializer_class = CropSerializer
     queryset = Crop.objects.all()
 
-    def get(self, request: Request) -> CustomResponse:
-        serializer = self.get_serializer(self.get_queryset(), many=True)
+    def get(self, request: Request, *args: Any, **kwargs: Any) -> CustomResponse:
+        if "pk" in self.kwargs:
+            response = self.retrieve(request, *args, **kwargs)
+        else:
+            response = self.list(request, *args, **kwargs)
+
+        return CustomResponse(response_data=response.data, status=status.HTTP_200_OK)
+
+    def post(self, request: Request, *args: Any, **kwargs: Any) -> CustomResponse:
+        response = self.create(request, args, kwargs)
 
         return CustomResponse(
-            response_data=serializer.data, status=status.HTTP_200_OK
+            response_data=response.data, status=status.HTTP_201_CREATED
+        )
+
+    def put(self, request: Request, *args: Any, **kwargs: Any) -> CustomResponse:
+        response = self.update(request, args, kwargs)
+
+        return CustomResponse(response_data=response.data, status=status.HTTP_200_OK)
+
+    def patch(self, request: Request, *args: Any, **kwargs: Any) -> CustomResponse:
+        response = self.partial_update(request, args, kwargs)
+
+        return CustomResponse(response_data=response.data, status=status.HTTP_200_OK)
+
+    def delete(self, request: Request, *args: Any, **kwargs: Any) -> CustomResponse:
+        response = self.destroy(request, args, kwargs)
+
+        return CustomResponse(
+            response_data=response.data, status=status.HTTP_204_NO_CONTENT
         )
