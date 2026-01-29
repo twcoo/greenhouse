43ef7b2b7a253eb2b1dbd09c672f2c5e25a4efd4
twcoo <mflesterc@gmail.com>
2026-01-30 00:17:29 +0800

    feat: remove custom crop response serializer

--- app/greenhouse/serializers/__init__.py
+++ app/greenhouse/serializers/__init__.py
@@ -1,4 +1,2 @@
 from .auth import RegisterSerializer as RegisterSerializer
-from .crop import CropListResponseSerializer as CropListResponseSerializer
-from .crop import CropResponseSerializer as CropResponseSerializer
-from .crop import CropSerializer as CropSerializer
+from .crops import CropSerializer as CropSerializer
---
+++ app/greenhouse/serializers/crops.py
@@ -0,0 +1,52 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
+
+from ..models import Crop
+
+
+class CropSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(
+        read_only=True, help_text="Unique identifier of the crop"
+    )
+    name = serializers.CharField(
+        max_length=50, help_text="Common name of the crop"
+    )
+    scientific_name = serializers.CharField(
+        max_length=100, help_text="Scientific (Latin) name of the crop"
+    )
+    category = serializers.ChoiceField(
+        choices=Crop.CATEGORY_CHOICES,
+        default="VEGETABLE",
+        help_text="Category of the crop",
+    )
+    sunlight_requirement = serializers.ChoiceField(
+        choices=Crop.SUNLIGHT_REQUIREMENT_CHOICES,
+        default="FULL SUN",
+        help_text="Sunlight requirement of the crop",
+    )
+    min_days_to_harvest = serializers.IntegerField(
+        min_value=1, help_text="Estimated minimum number of days until harvest"
+    )
+    max_days_to_harvest = serializers.IntegerField(
+        min_value=1, help_text="Estimated maximum number of days until harvest"
+    )
+
+    class Meta:
+        model = Crop
+        fields = (
+            "id",
+            "name",
+            "scientific_name",
+            "category",
+            "sunlight_requirement",
+            "min_days_to_harvest",
+            "max_days_to_harvest",
+        )
+
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Crop.objects.all(),
+                fields=["name", "scientific_name"],
+                message="Record already exist.",
+            )
+        ]
