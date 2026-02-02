46ce97ad479a617b486098858fae82e87accbfbd
twcoo <mflesterc@gmail.com>
2026-02-02 16:29:51 +0800

    test: setup crop-list-create endpoint

--- app/greenhouse/tests/__init__.py
+++ app/greenhouse/tests/__init__.py
@@ -1,3 +1,4 @@
 from .auth import AuthLoginTests as AuthLoginTests
 from .auth import AuthLogoutTests as AuthLogoutTests
 from .auth import AuthRegisterTests as AuthRegisterTests
+from .crops import CropListApiViewTests as CropListApiViewTests
