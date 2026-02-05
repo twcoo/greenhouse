e53d1808e12670c5058d530ce6a06804b60a1fa8
twcoo <mflesterc@gmail.com>
2026-02-05 08:45:51 +0800

    feat: add api version in path

--- app/core/settings.py
+++ app/core/settings.py
@@ -146,3 +146,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+API_VERSION = "v1"
--- app/core/urls.py
+++ app/core/urls.py
@@ -1,15 +1,22 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/docs",
+        f"api/{settings.API_VERSION}/schema/",
+        SpectacularAPIView.as_view(),
+        name="schema",
+    ),
+    path(
+        f"api/{settings.API_VERSION}/docs",
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
-    path("api/auth/", include("greenhouse.urls.auth")),
-    path("api/crops/", include("greenhouse.urls.crops")),
+    path(f"api/{settings.API_VERSION}/auth/", include("greenhouse.urls.auth")),
+    path(
+        f"api/{settings.API_VERSION}/crops/", include("greenhouse.urls.crops")
+    ),
 ]
