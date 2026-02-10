6d82aaead33d96a376987da9bb9a43a671c320c7
twcoo <mflesterc@gmail.com>
2026-02-10 09:49:09 +0800

    feat: validate unique name or scientific_name

--- app/greenhouse/serializers/crops.py
+++ app/greenhouse/serializers/crops.py
@@ -1,6 +1,6 @@
 from drf_spectacular.utils import extend_schema_serializer
 from rest_framework import serializers
-from rest_framework.validators import UniqueTogetherValidator
+from rest_framework.validators import UniqueTogetherValidator, UniqueValidator
 
 from ..models import Crop
 from ..openapi.examples import CROP_SERIALIZER_EXAMPLE
@@ -11,9 +11,25 @@ class CropSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(
         read_only=True, help_text="Unique identifier of the crop"
     )
-    name = serializers.CharField(max_length=50, help_text="Common name of the crop")
+    name = serializers.CharField(
+        max_length=50,
+        help_text="Common name of the crop",
+        validators=[
+            UniqueValidator(
+                queryset=Crop.objects.all(),
+                message="A crop with this name already exists.",
+            )
+        ],
+    )
     scientific_name = serializers.CharField(
-        max_length=100, help_text="Scientific (Latin) name of the crop"
+        max_length=100,
+        help_text="Scientific (Latin) name of the crop",
+        validators=[
+            UniqueValidator(
+                queryset=Crop.objects.all(),
+                message="A crop with this scientific name already exists.",
+            )
+        ],
     )
     category = serializers.ChoiceField(
         choices=Crop.CATEGORY_CHOICES, help_text="Category of the crop"
@@ -33,9 +49,15 @@ class CropSerializer(serializers.ModelSerializer):
         if value.isdigit():
             raise serializers.ValidationError("Name must be a valid string.")
 
+        return value
+
     def validate_scientific_name(self, value):
         if value.isdigit():
-            raise serializers.ValidationError("Scientific name must be a valid string.")
+            raise serializers.ValidationError(
+                "Scientific name must be a valid string."
+            )
+
+        return value
 
     class Meta:
         model = Crop
