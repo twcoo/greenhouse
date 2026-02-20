687df066d7a1ad184d31d4cb68cce443e1d4b506
twcoo <mflesterc@gmail.com>
2026-02-21 07:51:02 +0800

    feat: list and creat planting location endpoints

--- app/greenhouse/views/planting_location.py
+++ app/greenhouse/views/planting_location.py
@@ -7,32 +7,45 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 
 from ..models import PlantingLocation
+from ..openapi.examples import (
+    CREATE_PLANTING_LOCATION_REQUEST_GROUND_EXAMPLE,
+    CREATE_PLANTING_LOCATION_REQUEST_POT_EXAMPLE)
+from ..openapi.responses import (PLANTING_LOCATION_CREATE_VALIDATION_RESPONSE,
+                                 PLANTING_LOCATION_CREATED_RESPONSE,
+                                 PLANTING_LOCATION_LIST_RESPONSE)
 from ..serializers import PlantingLocationSerializer
 from ..utils.api import CustomAuthentication, CustomResponse
 
 
-# @extend_schema_view(
-#     get=extend_schema(
-#         tags=["Crops"],
-#         summary="List crops",
-#         description="Retrieve all crop records.",
-#         responses={
-#             200: CROP_LIST_RESPONSE,
-#         },
-#     ),
-#     post=extend_schema(
-#         tags=["Crops"],
-#         summary="Create crop",
-#         description="Create a new crop record.",
-#         examples=[
-#             CREATE_CROP_REQUEST_EXAMPLE,
-#         ],
-#         responses={
-#             201: CROP_CREATED_RESPONSE,
-#             400: CROP_CREATE_VALIDATION_RESPONSE,
-#         },
-#     ),
-# )
+@extend_schema_view(
+    get=extend_schema(
+        tags=["Planting Location"],
+        summary="List planting locations",
+        description=(
+            "Returns a paginated list of planting location records associated with the "
+            "currently authenticated user. Results are scoped per user."
+        ),
+        responses={
+            200: PLANTING_LOCATION_LIST_RESPONSE,
+        },
+    ),
+    post=extend_schema(
+        tags=["Planting Location"],
+        summary="Create planting location",
+        description=(
+            "Create a new planting location record. The location will automatically "
+            "be associated with the authenticated user."
+        ),
+        examples=[
+            CREATE_PLANTING_LOCATION_REQUEST_GROUND_EXAMPLE,
+            CREATE_PLANTING_LOCATION_REQUEST_POT_EXAMPLE,
+        ],
+        responses={
+            201: PLANTING_LOCATION_CREATED_RESPONSE,
+            400: PLANTING_LOCATION_CREATE_VALIDATION_RESPONSE,
+        },
+    ),
+)
 class PlantingLocationListApiView(
     mixins.ListModelMixin,
     mixins.CreateModelMixin,
@@ -42,10 +55,12 @@ class PlantingLocationListApiView(
     permission_classes = [IsAuthenticated]
 
     serializer_class = PlantingLocationSerializer
-    # queryset = PlantingLocation.objects.all()
 
     def get_queryset(self):
         return PlantingLocation.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
     def get(
         self, request: Request, *args: Any, **kwargs: Any
