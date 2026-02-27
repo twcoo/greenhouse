8b51638d26b5a0c807ad4137ce1b85faf89a193b
twcoo <mflesterc@gmail.com>
2026-02-27 17:02:45 +0800

    feat: upload crop image endpoint

--- app/greenhouse/urls/crops.py
+++ app/greenhouse/urls/crops.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from ..views import CropDetailAPIView, CropListApiView
+from ..views import CropDetailAPIView, CropListApiView, CropUploadImageView
 
 urlpatterns = [
     # List and create
     path("", CropListApiView.as_view(), name="crop-list-create"),
     # Get, partial update, full update, and delete
     path("<int:pk>", CropDetailAPIView.as_view(), name="crop-detail"),
+    # Upload image
+    path(
+        "<int:pk>/image/",
+        CropUploadImageView.as_view(),
+        name="crop-image-upload",
+    ),
 ]
