7cc42ee222894c31e53c8219823a25ee018983fc
twcoo <mflesterc@gmail.com>
2026-03-08 19:55:19 +0800

    chore: install tailwind and shadcnvue

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   root: "frontend",
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
