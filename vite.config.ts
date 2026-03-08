332d790d7ddec034581d6cb0e1ffbe4bdd6e8c2b
twcoo <mflesterc@gmail.com>
2026-03-08 20:54:14 +0800

    feat: set shadcnvue components destination folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,12 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   root: "frontend",
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './frontend/src'),
     },
   },
 })
