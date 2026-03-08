d0bf41ecb2e4c92d62f984b210be3be3b38e0c66
twcoo <mflesterc@gmail.com>
2026-03-08 22:03:34 +0800

    feat: setup initial views and routing

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,29 @@
+import {
+  createRouter,
+  createWebHistory,
+  type RouteRecordRaw,
+} from "vue-router";
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/login/index.vue"),
+  },
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    component: () => import("@/views/dashboard/index.vue"),
+  },
+  {
+    path: "/",
+    redirect: "/login",
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
