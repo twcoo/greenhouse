82881dee837f2abffce389b8ba6f383af5e8cf18
twcoo <mflesterc@gmail.com>
2026-03-11 18:48:11 +0800

    feat: setup status url

--- backend/greenhouse/urls/setup.py
+++ backend/greenhouse/urls/setup.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from ..views import SetupAdminView
+from ..views import SetupAdminView, SetupStatusView
 
 urlpatterns = [
     path("admin", SetupAdminView.as_view(), name="setup_admin"),
+    path("status", SetupStatusView.as_view(), name="setup_status"),
 ]
