298865654409590635d999cd04ff74437274f274
twcoo <mflesterc@gmail.com>
2026-03-13 15:37:29 +0800

    chore: install zod

--- frontend/src/views/setup/index.vue
+++ frontend/src/views/setup/index.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import SignupForm from "@/components/SignupForm.vue"
+import SetupAdminForm from '@/components/SetupAdminForm.vue';
 </script>
 
 <template>
   <div class="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
     <div class="w-full max-w-sm">
-      <SignupForm />
+      <SetupAdminForm />
     </div>
   </div>
 </template>
