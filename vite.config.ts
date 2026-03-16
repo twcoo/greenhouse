aaae025c101bac8d99b56000d53af4a177363faf
twcoo <mflesterc@gmail.com>
2026-03-16 22:15:59 +0800

    chore: setup tests in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,13 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   root: "frontend",
   plugins: [vue(), tailwindcss()],
+  test: {
+    environment: "jsdom",
+    globals: true
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './frontend/src'),
-    },
-  },
+    }
+  }
 })
