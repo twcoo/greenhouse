ca7ca498a87a12bfd469da0c54424baf30f55443
twcoo <mflesterc@gmail.com>
2026-03-19 20:32:31 +0800

    feat: initial login view and form

---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+</script>
+
+<template>
+  <div class="flex flex-col gap-6">
+    <Card>
+      <CardHeader>
+        <CardTitle>Login to your account</CardTitle>
+        <CardDescription> Enter your username below to login to your account </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <FieldGroup>
+            <Field>
+              <FieldLabel for="username"> Username </FieldLabel>
+              <Input id="username" type="text" placeholder="jmiller" required />
+            </Field>
+            <Field>
+              <div class="flex items-center">
+                <FieldLabel for="password"> Password </FieldLabel>
+              </div>
+              <Input id="password" type="password" required />
+            </Field>
+            <Field>
+              <Button type="submit"> Login </Button>
+            </Field>
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
+  </div>
+</template>
--- frontend/src/views/login/index.vue
+++ frontend/src/views/login/index.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
-import { Button } from "@/components/ui/button"
+import LoginForm from "@/components/LoginForm.vue"
+import { GalleryVerticalEnd } from "lucide-vue-next"
 </script>
 
 <template>
-  <Button>Hello Login</Button>
+  <div class="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div class="flex w-full max-w-sm flex-col gap-6">
+      <a href="#" class="flex items-center gap-2 self-center font-medium">
+        <div
+          class="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md"
+        >
+          <GalleryVerticalEnd class="size-4" />
+        </div>
+        Greenhouse
+      </a>
+      <LoginForm />
+    </div>
+  </div>
 </template>
