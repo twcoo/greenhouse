4f11ed938b8907e029ac4b1a599294d3731ad16b
twcoo <mflesterc@gmail.com>
2026-03-21 23:39:55 +0800

    chore: reduce api client code

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -3,6 +3,9 @@ import axios from "axios"
 export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000,
+  withCredentials: true,
+  xsrfCookieName: "csrftoken",
+  xsrfHeaderName: "X-CSRFToken",
   headers: {
     "Content-Type": "application/json",
   },
