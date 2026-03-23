1f0f2ea177b401261f19422f9c6e62b3468dca79
twcoo <mflesterc@gmail.com>
2026-03-23 09:05:28 +0800

    feat: install shadcnvue dashboard

--- frontend/src/views/dashboard/index.vue
+++ frontend/src/views/dashboard/index.vue
@@ -1,7 +1,27 @@
 <script setup lang="ts">
-import { Button } from "@/components/ui/button"
+import AppSidebar from "@/components/AppSidebar.vue"
+import SectionCards from "@/components/SectionCards.vue"
+import SiteHeader from "@/components/SiteHeader.vue"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 </script>
 
 <template>
-  <Button>Hello Dashboard</Button>
+  <SidebarProvider
+    :style="{
+      '--sidebar-width': 'calc(var(--spacing) * 72)',
+      '--header-height': 'calc(var(--spacing) * 12)',
+    }"
+  >
+    <AppSidebar variant="inset" />
+    <SidebarInset>
+      <SiteHeader />
+      <div class="flex flex-1 flex-col">
+        <div class="@container/main flex flex-1 flex-col gap-2">
+          <div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <SectionCards />
+          </div>
+        </div>
+      </div>
+    </SidebarInset>
+  </SidebarProvider>
 </template>
