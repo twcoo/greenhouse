86d87ffe9ae1d15b37402e52205eb61d4b2e5b9b
twcoo <mflesterc@gmail.com>
2026-03-24 19:48:41 +0800

    feat: use interceptors for setting csrftoken header

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,12 +1,21 @@
 import axios from "axios"
+import Cookies from "js-cookie"
 
 export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000,
   withCredentials: true,
-  xsrfCookieName: "csrftoken",
-  xsrfHeaderName: "X-CSRFToken",
   headers: {
     "Content-Type": "application/json",
   },
+})
+
+apiClient.interceptors.request.use((config) => {
+  const token = Cookies.get("csrftoken")
+
+  if (token) {
+    config.headers["X-CSRFToken"] = token
+  }
+
+  return config
 })
