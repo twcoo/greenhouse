7a1887e82c916a4deea8b41179234991d7aa993f
twcoo <mflesterc@gmail.com>
2026-03-25 08:46:04 +0800

    chore: lint codes

--- frontend/src/components/NavUser.vue
+++ frontend/src/components/NavUser.vue
@@ -21,7 +21,7 @@ defineProps<{
   user: User
 }>()
 
-const emit = defineEmits(['logout'])
+const emit = defineEmits(["logout"])
 
 const { isMobile } = useSidebar()
 
@@ -47,8 +47,10 @@ const handleLogout = () => {
     <SidebarMenuItem>
       <DropdownMenu>
         <DropdownMenuTrigger as-child>
-          <SidebarMenuButton size="lg"
-            class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
+          <SidebarMenuButton
+            size="lg"
+            class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+          >
             <Avatar class="h-8 w-8 rounded-lg grayscale">
               <AvatarFallback class="rounded-lg">{{ getInitials(user?.username) }}</AvatarFallback>
             </Avatar>
@@ -58,14 +60,18 @@ const handleLogout = () => {
             <IconDotsVertical class="ml-auto size-4" />
           </SidebarMenuButton>
         </DropdownMenuTrigger>
-        <DropdownMenuContent class="w-(--reka-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-          :side="isMobile ? 'bottom' : 'right'" :side-offset="4" align="end">
+        <DropdownMenuContent
+          class="w-(--reka-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+          :side="isMobile ? 'bottom' : 'right'"
+          :side-offset="4"
+          align="end"
+        >
           <DropdownMenuLabel class="p-0 font-normal">
             <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
               <Avatar class="h-8 w-8 rounded-lg">
                 <AvatarFallback class="rounded-lg">{{
                   getInitials(user?.username)
-                  }}</AvatarFallback>
+                }}</AvatarFallback>
               </Avatar>
               <div class="grid flex-1 text-left text-sm leading-tight">
                 <span class="truncate font-medium">{{ user?.username }}</span>
--- frontend/src/views/login/index.vue
+++ frontend/src/views/login/index.vue
@@ -7,7 +7,9 @@ import { IconHomeEco } from "@tabler/icons-vue"
   <div class="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
     <div class="flex w-full max-w-sm flex-col gap-6">
       <a href="#" class="flex items-center gap-2 self-center font-medium">
-        <div class="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+        <div
+          class="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md"
+        >
           <IconHomeEco class="!size-5" />
         </div>
         Greenhouse
