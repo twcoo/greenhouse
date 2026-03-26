4fa06adf789da868772a3ea2212a62cef8b3b423
twcoo <mflesterc@gmail.com>
2026-03-26 08:45:33 +0800

    feat: make slot spacing standard

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -14,12 +14,8 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
     <SidebarInset>
       <SiteHeader />
 
-      <div class="flex flex-1 flex-col">
-        <div class="@container/main flex flex-1 flex-col gap-2">
-          <div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-            <slot />
-          </div>
-        </div>
+      <div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6 px-4 md:px-6">
+        <slot />
       </div>
     </SidebarInset>
   </SidebarProvider>
