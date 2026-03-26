91e167c2f78708c6efb7d8abe1780492627c8b0c
twcoo <mflesterc@gmail.com>
2026-03-26 08:15:05 +0800

    feat: use app layout in views

---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import AppSidebar from "@/components/AppSidebar.vue"
+import SiteHeader from "@/components/SiteHeader.vue"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+</script>
+
+<template>
+  <SidebarProvider :style="{
+    '--sidebar-width': 'calc(var(--spacing) * 72)',
+    '--header-height': 'calc(var(--spacing) * 12)',
+  }">
+    <AppSidebar variant="inset" />
+
+    <SidebarInset>
+      <SiteHeader />
+
+      <div class="flex flex-1 flex-col">
+        <div class="@container/main flex flex-1 flex-col gap-2">
+          <div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <slot />
+          </div>
+        </div>
+      </div>
+    </SidebarInset>
+  </SidebarProvider>
+</template>
--- frontend/src/views/dashboard/index.vue
+++ frontend/src/views/dashboard/index.vue
@@ -1,27 +1,10 @@
 <script setup lang="ts">
-import AppSidebar from "@/components/AppSidebar.vue"
+import AppLayout from "@/layouts/AppLayout.vue"
 import SectionCards from "@/components/SectionCards.vue"
-import SiteHeader from "@/components/SiteHeader.vue"
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 </script>
 
 <template>
-  <SidebarProvider
-    :style="{
-      '--sidebar-width': 'calc(var(--spacing) * 72)',
-      '--header-height': 'calc(var(--spacing) * 12)',
-    }"
-  >
-    <AppSidebar variant="inset" />
-    <SidebarInset>
-      <SiteHeader />
-      <div class="flex flex-1 flex-col">
-        <div class="@container/main flex flex-1 flex-col gap-2">
-          <div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-            <SectionCards />
-          </div>
-        </div>
-      </div>
-    </SidebarInset>
-  </SidebarProvider>
+  <AppLayout>
+    <SectionCards />
+  </AppLayout>
 </template>
