aca28ee0004c699373dbc12b46d8ab64a2675f41
twcoo <mflesterc@gmail.com>
2026-03-27 08:48:05 +0800

    chore: create crops type

---
+++ frontend/src/types/crop.ts
@@ -0,0 +1,9 @@
+export interface Crop {
+  id: number
+  name: string
+  scientific_name: string
+  category: string
+  sunlight_requirement: string
+  min_days_to_harvest: number
+  max_days_to_harvest: number
+}
