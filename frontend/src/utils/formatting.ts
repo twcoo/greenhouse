68f7b69642ce4e9ea225b980f4a172a7cb0255b3
twcoo <mflesterc@gmail.com>
2026-03-27 08:48:23 +0800

    feat: toTitleCase util

---
+++ frontend/src/utils/formatting.ts
@@ -0,0 +1,7 @@
+export function toTitleCase(str: string): string {
+  return str
+    // replace underscores with spaces
+    .replace(/_/g, ' ')
+    // capitalize first letter of each word
+    .replace(/\b\w/g, (char) => char.toUpperCase());
+}
