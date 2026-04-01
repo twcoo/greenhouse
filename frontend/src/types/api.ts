b57ae869d7cd3fddbed2bfaa4d927d756af9cb4b
twcoo <mflesterc@gmail.com>
2026-04-01 10:10:03 +0800

    chore: add paginated type response

--- frontend/src/types/api.ts
+++ frontend/src/types/api.ts
@@ -9,3 +9,12 @@ export interface APIErrorResponse {
   data: null
   message: Record<string, string[]> | string
 }
+
+export type PaginatedResponse<T> = {
+  count: number
+  next: string | null
+  previous: string | null
+  results: T[]
+}
+
+export type PaginatedAPIResponse<T> = APIResponse<PaginatedResponse<T>>
