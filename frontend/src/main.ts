f3692f5fa67c2da0f332f787aef2874e8944f7d5
twcoo <mflesterc@gmail.com>
2026-04-02 11:43:57 +0800

    chore: setup vue query

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,10 +3,23 @@ import { createPinia } from "pinia"
 import "@/style.css"
 import App from "@/App.vue"
 import router from "@/router"
+import { VueQueryPlugin, type VueQueryPluginOptions } from '@tanstack/vue-query'
 
 const pinia = createPinia()
 const app = createApp(App)
 
+const vueQueryOptions: VueQueryPluginOptions = {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        retry: 1,
+      },
+    },
+  },
+}
+
+app.use(VueQueryPlugin, vueQueryOptions)
 app.use(pinia)
 app.use(router)
 app.mount("#app")
