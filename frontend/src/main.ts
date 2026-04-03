76fc2e4d24ac2d9e4260aaffe1ce347110943a23
twcoo <mflesterc@gmail.com>
2026-04-03 19:30:18 +0800

    chore: lint codes

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from "pinia"
 import "@/style.css"
 import App from "@/App.vue"
 import router from "@/router"
-import { VueQueryPlugin, type VueQueryPluginOptions } from '@tanstack/vue-query'
+import { VueQueryPlugin, type VueQueryPluginOptions } from "@tanstack/vue-query"
 
 const pinia = createPinia()
 const app = createApp(App)
