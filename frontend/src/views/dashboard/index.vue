fe5df7661e19b6788e0029f510a730faba338f54
twcoo <mflesterc@gmail.com>
2026-04-11 05:07:04 +0800

    chore: remove unused section card

--- frontend/src/views/dashboard/index.vue
+++ frontend/src/views/dashboard/index.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import AppLayout from "@/layouts/AppLayout.vue"
-import SectionCards from "@/components/SectionCards.vue"
 </script>
 
 <template>
   <AppLayout>
-    <SectionCards />
+    <div>In development</div>
   </AppLayout>
 </template>
