b9d4ba5635fd0876a1ee599aec696a5f48263f4b
twcoo <mflesterc@gmail.com>
2026-05-04 18:13:35 +0800

    feat: use validate image file util

--- backend/greenhouse/serializers/planting_location_status.py
+++ backend/greenhouse/serializers/planting_location_status.py
@@ -1,11 +1,10 @@
-import os
-
 from drf_spectacular.utils import extend_schema_serializer
 from rest_framework import serializers
 
 from ..models import PlantingLocationStatus
 from ..openapi.planting_location_status.examples import \
     PLANTING_LOCATION_STATUS_SERIALIZER_EXAMPLE
+from .utils import validate_image_file
 
 
 @extend_schema_serializer(
@@ -38,25 +37,7 @@ class PlantingLocationStatusSerializer(serializers.ModelSerializer):
     def validate_image(self, value):
         if value is None:
             return value
-
-        ext = os.path.splitext(value.name)[1].lower()
-        valid_extensions = [".jpg", ".jpeg", ".png"]
-
-        if ext not in valid_extensions:
-            raise serializers.ValidationError(
-                "Unsupported file extension. Please upload a "
-                ".jpg or .png image."
-            )
-
-        filesize_limit = 2 * 1024 * 1024
-
-        if value.size > filesize_limit:
-            limit_mb = filesize_limit / (1024 * 1024)
-            raise serializers.ValidationError(
-                f"File too large. Size should not exceed {limit_mb}MB."
-            )
-
-        return value
+        return validate_image_file(value)
 
     class Meta:
         model = PlantingLocationStatus
