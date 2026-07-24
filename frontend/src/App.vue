f0df919379378b8482aa4bee6d89a0583625960c
twcoo <mflesterc@gmail.com>
2026-07-24 19:52:35 +0800

    chore: install and setup sonner/toast

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+import "vue-sonner/style.css"
+import { Toaster } from "@/components/ui/sonner"
+</script>
+
 <template>
   <main>
     <RouterView />
+    <Toaster />
   </main>
 </template>
